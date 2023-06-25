74222f43e35341c3b1d44fcdd6f82a762a46cf76
github user tmcoco0228si <proguteinitar3150@gmail.com>
2023-06-25 10:36:34 +0900

    finished

--- README.md
+++ README.md
@@ -1,7 +1,98 @@
 # react-three-fiber-app
 
----
-
 ### 使用ライブラリ
 
-npm i three react-three-fiber @react-three/drei
+## npm i three react-three-fiber @react-three/drei
+
+# React Three Fiber and Drei Components
+
+このソースコードは React Three Fiber と Drei を用いた 3D ビジュアルの作成例です。
+
+## 使用コンポーネントとプロパティの説明
+
+### PresentationControls
+
+ユーザーによるインタラクティブな制御を可能にするコンポーネントです。`global`, `config`, `snap`などのプロパティを持ちます。
+
+```jsx
+<PresentationControls
+  global
+  config={{ mass: 2, tension: 400 }}
+  snap={{ mass: 4, tension: 300 }}
+/>
+```
+
+---
+
+### Float
+
+オブジェクトを揺らすためのコンポーネントです。`rotationIntensity`プロパティで揺れの強度を制御します。
+
+```jsx
+<Float rotationIntensity={1.5} />
+```
+
+---
+
+### rectAreaLight
+
+特定の矩形領域から放出される光を模擬します。`color`プロパティで光の色、`intensity`プロパティで光の強度、`rotation`プロパティで光源の回転、`width`と`height`で光源の大きさ、`position`プロパティで光源の位置を制御します。
+
+```jsx
+<rectAreaLight
+  color={"#0021a7"}
+  intensity={55}
+  rotation={[0.1, Math.PI, 0]}
+  width={2.0}
+  height={1.65}
+  position={[0, 0, -1]}
+/>
+```
+
+---
+
+### primitive
+
+3D オブジェクトをシーンに配置するためのコンポーネントです。`object`プロパティには表示したい 3D オブジェクトを、`position`プロパティにはオブジェクトの位置を指定します。
+
+```jsx
+<primitive object={macbook.scene} position={[0, -1.5, 0]} />
+```
+
+---
+
+### Html
+
+3D オブジェクト上に HTML 要素を配置するためのコンポーネントです。`position`プロパティで要素の位置、`distanceFactor`で要素の大きさ、`rotation-x`で要素の X 軸回転を制御します。`transform`プロパティは、要素が 3D 空間内で正しく表示されるようにします。`wrapperClass`には CSS クラス名を指定します。
+
+```jsx
+<Html
+  position={[0, 1.56, -1.4]}
+  distanceFactor={1.17}
+  rotation-x={-0.256}
+  transform
+  wrapperClass="htmlScreen"
+>
+```
+
+---
+
+### Text
+
+3D 空間内にテキストを配置するためのコンポーネントです。`font`プロパティで使用するフォントを、`fontSize`プロパティでフォントサイズを、`position`プロパティでテキストの位置を指定します。
+
+```jsx
+<Text font="RobotoSlab-Bold.ttf" fontSize={0.6} position={[0, 1.7, 0.75]}>
+  React-three-fiber
+</Text>
+```
+
+---
+
+### ContactShadows
+
+接触するオブジェクト間に影を生成するコンポーネントです。`scale`プロパティで影の大きさ、blur プロパティで影のぼかしの度合い、`position-y`プロパティで影の位置（Y 軸）、`opacity`プロパティで影の不透明度を制御します。
+
+```jsx
+<ContactShadows scale={7} blur={2.4} position-y={-2.0} opacity={0.7} />
+```
--- r3f-portfolio-tutorial/src/App.jsx
+++ r3f-portfolio-tutorial/src/App.jsx
@@ -1,35 +1,116 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/* eslint-disable react/no-unknown-property */
+import "./App.css";
+// three.jsのシーンをReactのCanvas内に描画するためのコンポーネント
+import { Canvas } from "@react-three/fiber";
+import {
+  ContactShadows, // オブジェクトに接触しているシャドウを描画
+  Float, // 等速回転
+  Html, // 3Dオブジェクト上にHTML要素を描画
+  PresentationControls, // three.jsのシーンにおけるユーザーインタラクションを管理するコンポーネント
+  Text, // 3Dテキストを描画
+  useGLTF, // GLTF形式の3Dモデルをロード
+} from "@react-three/drei";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const macbook = useGLTF(
+    "https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf"
+  );
+  const coffee = useGLTF(
+    "https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/cup-tea/model.gltf"
+  );
+  const donut = useGLTF("./donut.gltf");
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {/* fov:カメラ角度  near,far: nearからfarまでの距離をカメラで映す  */}
+        <Canvas
+          camera={{ fov: 45, near: 0.1, far: 2000, position: [0, 1.5, 6] }}
+        >
+          {/* eslint-disable-next-line react/no-unknown-property */}
+          <color args={["#1987CF"]} attach="background" />
+          <ambientLight />
+          <directionalLight position={[0, 0, 5]} intensity={1.4} />
+          <PresentationControls
+            global
+            config={{ mass: 2, tension: 400 }}
+            snap={{ mass: 4, tension: 300 }}
+          >
+            <Float rotationIntensity={1.5}>
+              <rectAreaLight
+                color={"#0021a7"}
+                intensity={55}
+                rotation={[0.1, Math.PI, 0]}
+                width={2.0}
+                height={1.65}
+                positon={[0, 0, -1]}
+              />
+              <primitive object={macbook.scene} position={[0, -1.5, 0]}>
+                <Html
+                  position={[0, 1.56, -1.4]}
+                  distanceFactor={1.17}
+                  rotation-x={-0.256}
+                  transform
+                  wrapperClass="htmlScreen"
+                >
+                  <iframe
+                    src=" https://www.trailstem.dev"
+                    frameboder="0"
+                  ></iframe>
+                </Html>
+              </primitive>
+            </Float>
+
+            <Float rotationIntensity={1.5}>
+              <rectAreaLight
+                color={"#0021a7"}
+                intensity={55}
+                rotation={[0.1, Math.PI, 0]}
+                width={2.0}
+                height={1.65}
+                positon={[0, 0, -1]}
+              />
+              <primitive
+                object={coffee.scene}
+                position={[-2.5, -1.3, 0.2]}
+                scale={[2, 2, 2]}
+              />
+            </Float>
+
+            <Float rotationIntensity={1.5}>
+              <rectAreaLight
+                color={"#0021a7"}
+                intensity={55}
+                rotation={[0.1, Math.PI, 0]}
+                width={2.0}
+                height={1.65}
+                positon={[0, 0, -1]}
+              />
+              <primitive
+                object={donut.scene}
+                position={[2.4, -1.3, 0.2]}
+                scale={[3, 3, 3]}
+                rotation-x={0.4}
+              />
+            </Float>
+
+            <Text
+              font="RobotoSlab-Bold.ttf"
+              fontSize={0.6}
+              position={[0, 1.7, 0.75]}
+            >
+              React-thee-fiber
+            </Text>
+          </PresentationControls>
+          <ContactShadows
+            scale={7}
+            blur={2.4}
+            position-y={-2.0}
+            opacity={0.7}
+          />
+        </Canvas>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
